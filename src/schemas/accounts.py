d48ed32930cbc5733aba6d7c8c12d5ebffb753e3
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-04-18 08:57:58 +0300

    feat: create TokenRefreshRequestSchema and TokenRefreshResponseSchema schemas

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -56,3 +56,12 @@ class UserLoginResponseSchema(BaseModel):
     access_token: str
     refresh_token: str
     token_type: str = "bearer"
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
