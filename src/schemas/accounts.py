5881669849285637759931fd11aa5cfc97e7c009
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-04-18 08:38:16 +0300

    feat: create BaseEmailPasswordSchema schema

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -1,6 +1,20 @@
-from pydantic import BaseModel, EmailStr, field_validator
+from pydantic import BaseModel, EmailStr, field_validator, ConfigDict
 
 from database import accounts_validators
 
 
-# Write your code here
+class BaseEmailPasswordSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    email: EmailStr
+    password: str
+
+    @field_validator("email")
+    @classmethod
+    def validate_email(cls, value: str):
+        return accounts_validators.validate_email(value)
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value: str):
+        return accounts_validators.validate_password_strength(value)
