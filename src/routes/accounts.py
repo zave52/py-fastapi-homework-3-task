721b787b480933d3885b16dec16abbcc1a8c281c
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-04-18 12:12:23 +0300

    feat: create register_user endpoint

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -18,8 +18,85 @@ from database import (
     RefreshTokenModel
 )
 from exceptions import BaseSecurityError
+from schemas import (
+    UserRegistrationResponseSchema,
+    UserRegistrationRequestSchema
+)
 from security.interfaces import JWTAuthManagerInterface
 
 router = APIRouter()
 
-# Write your code here
+
+@router.post(
+    "/register/",
+    response_model=UserRegistrationResponseSchema,
+    status_code=201,
+    responses={
+        409: {
+            "description": "Conflict",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "A user with this email "
+                                  "test@example.com already exists."
+                    }
+                }
+            }
+        },
+        500: {
+            "description": "Server error",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "An error occurred during user creation."
+                    }
+                }
+            }
+        }
+    }
+)
+async def register_user(
+    user_data: UserRegistrationRequestSchema,
+    db: AsyncSession = Depends(get_db)
+):
+    query_email = select(UserModel).where(UserModel.email == user_data.email)
+    result = await db.execute(query_email)
+    if result.scalar_one_or_none():
+        raise HTTPException(
+            status_code=409,
+            detail=f"A user with this email {user_data.email} already exists."
+        )
+
+    query_group = select(UserGroupModel).where(
+        UserGroupModel.name == UserGroupEnum.USER
+    )
+    result = await db.execute(query_group)
+    group = result.scalar_one_or_none()
+    if not group:
+        raise HTTPException(
+            status_code=500,
+            detail="Default user group not found"
+        )
+
+    try:
+        new_user = UserModel.create(
+            email=str(user_data.email),
+            raw_password=user_data.password,
+            group_id=group.id
+        )
+        db.add(new_user)
+        await db.flush()
+
+        activation_token = ActivationTokenModel(user_id=new_user.id)
+        db.add(activation_token)
+
+        await db.commit()
+        await db.refresh(new_user)
+    except SQLAlchemyError as e:
+        await db.rollback()
+        raise HTTPException(
+            status_code=500,
+            detail="An error occurred during user creation."
+        ) from e
+    else:
+        return UserRegistrationResponseSchema.model_validate(new_user)
